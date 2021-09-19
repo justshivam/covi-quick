dea6f0fd015bf5946106e27670ee2b406597a5df
Shivam Singh <stdnt.shivam@gmail.com>
2021-09-19 15:40:09 +0530

    Deploy Fix

--- covid/settings.py
+++ covid/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-from py_dotenv import read_dotenv
+# from py_dotenv import read_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-read_dotenv(dotenv_path)
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# read_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
